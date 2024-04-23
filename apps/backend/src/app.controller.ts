8a97c2b3c79e9dba516d706e2fd950fd0e57be5b
HazySoda <wildnode@gmail.com>
2024-04-23 22:41:13 +0400

    feat(backend): setup prisma

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Post } from '@nestjs/common'
 import { AppService } from './app.service'
 
 @Controller()
@@ -8,5 +8,18 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello()
+  }
+
+  @Get('users')
+  async getUsers() {
+    return this.appService.users({})
+  }
+
+  @Post('users')
+  async createUser() {
+    return this.appService.createUser({
+      email: 'test@example.com',
+      name: 'John Doe',
+    })
   }
 }
--- apps/backend/src/app.service.ts
+++ apps/backend/src/app.service.ts
@@ -1,8 +1,35 @@
 import { Injectable } from '@nestjs/common'
+import { Prisma, User } from '@prisma/client'
+import { PrismaService } from './prisma.service'
 
 @Injectable()
 export class AppService {
+  constructor(private readonly prismaService: PrismaService) {}
+
   getHello(): string {
     return 'Hello World!'
+  }
+
+  async users(params: {
+    skip?: number
+    take?: number
+    cursor?: Prisma.UserWhereUniqueInput
+    where?: Prisma.UserWhereInput
+    orderBy?: Prisma.UserOrderByWithRelationInput
+  }): Promise<User[]> {
+    const { skip, take, cursor, where, orderBy } = params
+    return this.prismaService.user.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy,
+    })
+  }
+
+  async createUser(data: Prisma.UserCreateInput): Promise<User> {
+    return this.prismaService.user.create({
+      data,
+    })
   }
 }
