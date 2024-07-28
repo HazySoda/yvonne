c658234af02b63297052280dbb9278bcd5554058
HazySoda <wildnode@gmail.com>
2024-07-28 11:57:58 +0400

    feat(*): add prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,21 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'avoid',
+  singleAttributePerLine: true,
+  plugins: ['prettier-plugin-organize-imports'],
+}
+
+export default config
